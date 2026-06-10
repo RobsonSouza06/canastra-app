ef1f5bd16e4b69121f9265a6433d742767a9396c
RobsonSouza06 <robsonsouza.lds@gmail.com>
2026-06-10 12:29:08 -0300

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+// Mudei para v2 para forçar a atualização no celular
+const CACHE_NAME = "canastra-cache-v2"; 
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
